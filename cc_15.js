2d1cf2898622a8df12098f7610282151a7aaaf13
Abdulhakim <Abdulthe383@gmail.com>
2025-03-16 18:06:26 -0400

    Task 1 - Base Structure Setup

---
+++ cc_15.js
@@ -0,0 +1,4 @@
+ // Task 1 - Creating the Base Structure // 
+function addRiskItem(riskName, riskLevel, department) {}
+const riskDashboard = document.getElementById("riskDashboard");// The dashboard
+    
